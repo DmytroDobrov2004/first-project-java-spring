5631b8b0ec5fc2a68b10f6fdc453f4d38293c466
DmytroDobrov2004 <ddobrov1@stu.vistula.edu.pl>
2023-02-03 13:24:13 +0100

    Initial commit

---
+++ src/main/java/pl/vistula/edu/firstprojectjavaspring/FirstProjectJavaSpringApplication.java
@@ -0,0 +1,15 @@
+package pl.vistula.edu.firstprojectjavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class FirstProjectJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectJavaSpringApplication.class, args);
+	}
+
+}
